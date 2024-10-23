65de1e77a03464e9983536eb773903a3e7f2e9e1
unknown <rkthakur941@gmail.com>
2024-10-23 22:47:35 +0530

    Adv and fetaures

---
+++ intro.java
@@ -0,0 +1,21 @@
+/* object oriented Programming(oops) 
+It is a methodology to develop and design the programs using class and object.
+
+Advanteges of object Oriented programming
+. Modularity
+. Reusability
+.flexibility
+
+
+features of oops
+. class
+.object
+.abstarction
+.encapsulatiion
+.Inheritance
+.Pollymorphism
+
+
+
+ 
+*/
